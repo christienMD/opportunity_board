6bfc2ecb982fb2557805cad9cbb5d6d72c382bdf
Md Christien <mdchristien12@gmail.com>
2024-05-23 22:57:57 +0100

    improve layout in the landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,7 +27,7 @@
 @endsection
 @section('main')
 <div class="">
-    <h3 class="md:container md:mx-auto text-base text-center px-6 w-full md:max-w-4xl">Explore exciting job opportunities from top companies. Whether you're a recent graduate or an experienced professional, we've got something for you.</h3>
+    {{-- <h3 class="md:container md:mx-auto text-base text-center px-6 w-full md:max-w-4xl">Explore exciting job opportunities from top companies. Whether you're a recent graduate or an experienced professional, we've got something for you.</h3> --}}
     @include('components.search_input_')
 
     <div class="flex justify-center">
